cd85bcc30f08e53d695ebf329569205db52d78bb
Jacopocasanova <jacopo.casanova.stud@ispascalcomandini.it>
2022-03-18 08:46:07 +0100

    aggiunti metodi alunno

--- es_Verifica/es_Verifica/Alunno.cs
+++ es_Verifica/es_Verifica/Alunno.cs
@@ -10,14 +10,34 @@ namespace es_Verifica
     {
         public string Nome { set; get; }
         public string Cognome { set; get; }
-        public string Eta { set; get; }
+        public int Eta { set; get; }
 
-        public Alunno (string nome, string cognome, string eta)
+        public Alunno (string nome, string cognome, int eta)
         {
             Nome = nome;
             Cognome = cognome;
             Eta = eta;
         }
 
+        public string CalcoloEta(string nome, string cognome, int eta)
+        {
+            string anno;
+            if (eta < 16)
+            {
+                 anno = "biennio";
+            }else
+            {
+                 anno = "triennio";
+            }
+
+            return anno;
+        }
+
+        public override string ToString()
+        {
+            string anno;
+            anno = CalcoloEta(Nome, Cognome, Eta);
+            return "L'alunno si chiama " + Nome + " " + Cognome + " ha " + Eta + " anni ed è del " + anno;
+        }
     }
 }
