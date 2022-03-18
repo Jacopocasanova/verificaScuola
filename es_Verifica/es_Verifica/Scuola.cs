749053750d48cbfe4d6f8d71775bc83256434e33
Jacopocasanova <jacopo.casanova.stud@ispascalcomandini.it>
2022-03-18 09:02:56 +0100

    aggiunta classe scuola

---
+++ es_Verifica/es_Verifica/Scuola.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_Verifica
+{
+    class Scuola
+    {
+        public string Nome { set; get; }
+        public string Indirizzo { set; get; }
+        public string OrarioChiusura { set; get; }
+        public string OrarioApertura { set; get; }
+
+        public Scuola ( string nome, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioApertura;
+        }
+    }
+}
