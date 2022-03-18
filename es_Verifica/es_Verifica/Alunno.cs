aef02763ad91d30f6637455728ca87494fe8f46a
Jacopocasanova <jacopo.casanova.stud@ispascalcomandini.it>
2022-03-18 08:35:43 +0100

    salvatsggio

--- es_Verifica/es_Verifica/Alunno.cs
+++ es_Verifica/es_Verifica/Alunno.cs
@@ -8,5 +8,16 @@ namespace es_Verifica
 {
     class Alunno
     {
+        public string Nome { set; get; }
+        public string Cognome { set; get; }
+        public string Eta { set; get; }
+
+        public Alunno (string nome, string cognome, string eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
     }
 }
